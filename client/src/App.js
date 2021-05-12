d7284d863ad89ae1735e93ebf018e0df02e501e4
jsolano <justinesolano2370@gmail.com>
2021-05-12 17:58:20 +0100

    redoing portfolio

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+
+
+const App = () => {
+
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
